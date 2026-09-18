bd1fb60ebf7c1a58c65954263c9b236e4dfaf394
asm-shad <asmshad@gmail.com>
2026-09-19 01:48:14 +0600

    Deployed backend added in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,26 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ai-resume-checker-backend-beryl.vercel.app",
         changeOrigin: true,
       },
     },
